5aa90f972d538a6dc0ed2558c93cc5e4cf95fb20
iamsr16 <83810443+iamsr16@users.noreply.github.com>
2023-01-23 17:23:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+
+def main():
+    try:
+        # Getting the current time upto seconds only.
+        cur_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print('Current Time : ',cur_time)
+    except:
+        print('Something Wrong in main function',exc_info=True)
+
+if __name__=='__main__':
+    main()
